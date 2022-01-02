3e242fa8b2df9c22cf5c9b45edd6eafcb837d540
Fabio Carpinato <fabiocarpinato@outlook.it>
2022-01-02 16:58:40 +0100

    creates a hello world contract

---
+++ contracts/_HelloWorld.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * @title HelloWorld
+ * @dev Get Hello World string
+ */
+contract HelloWorld {
+    string public helloWorld = "Hello World";
+}
+
